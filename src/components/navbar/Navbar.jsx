de61323521bd2d74f9eb8b7331a4fced1442d793
Grant <grantlee97@gmail.com>
2022-01-27 00:25:22 +0800

    Made some navbar buttons interactive

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -3,20 +3,25 @@ import "./Navbar.css"
 import grant_logo from "../../img/grant_logo_white.png"
 import { Icon } from '@iconify/react';
 
-function handleClick() {
-    console.log('in cardClick');
-} 
+const scrollToTop = () => {
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+    });
+};
 
 const Navbar = () => {
   return (
     <nav className="navbar">
         <div className="navbar-wrapper">
             <div className="navbar-left">
-                <img className="navbar-logo" src={grant_logo} alt=""/>
-                <Icon className="socials" icon="akar-icons:linkedin-fill" onClick={() => { window.open("https://www.linkedin.com/in/grantleeyx/", "_blank") }} />
-                <Icon className="socials" icon="akar-icons:github-fill" onClick={() => { window.open("https://github.com/grrrrnt", "_blank") }} />
-                <Icon className="socials" icon="akar-icons:instagram-fill" onClick={() => { window.open("https://www.instagram.com/grrrr_nt", "_blank") }} />
-                <Icon className="socials" icon="akar-icons:spotify-fill" onClick={() => { window.open("https://smarturl.it/music-by-grant", "_blank") }} />
+                <button onClick={scrollToTop} className="navbar-logo-button">
+                    <img className="navbar-logo" src={grant_logo} alt=""/>
+                </button>
+                <Icon className="socials-1" icon="akar-icons:linkedin-fill" onClick={() => { window.open("https://www.linkedin.com/in/grantleeyx/", "_blank") }} />
+                <Icon className="socials-2" icon="akar-icons:github-fill" onClick={() => { window.open("https://github.com/grrrrnt", "_blank") }} />
+                <Icon className="socials-3" icon="akar-icons:instagram-fill" onClick={() => { window.open("https://www.instagram.com/grrrr_nt", "_blank") }} />
+                <Icon className="socials-4" icon="akar-icons:spotify-fill" onClick={() => { window.open("https://smarturl.it/music-by-grant", "_blank") }} />
             </div>
             <div className="navbar-right">
                 <h2 className="navbar-right-item">Education</h2>
