e8fd3fceaef853d13b1446fafd543e3edd4e3cdd
Grant <grantlee97@gmail.com>
2022-01-26 23:53:28 +0800

    Added social links

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -3,16 +3,20 @@ import "./Navbar.css"
 import grant_logo from "../../img/grant_logo_white.png"
 import { Icon } from '@iconify/react';
 
+function handleClick() {
+    console.log('in cardClick');
+} 
+
 const Navbar = () => {
   return (
     <nav className="navbar">
         <div className="navbar-wrapper">
             <div className="navbar-left">
                 <img className="navbar-logo" src={grant_logo} alt=""/>
-                <Icon className="socials" icon="akar-icons:linkedin-fill" />
-                <Icon className="socials" icon="akar-icons:github-fill" />
-                <Icon className="socials" icon="akar-icons:instagram-fill" />
-                <Icon className="socials" icon="akar-icons:spotify-fill" />
+                <Icon className="socials" icon="akar-icons:linkedin-fill" onClick={() => { window.open("https://www.linkedin.com/in/grantleeyx/", "_blank") }} />
+                <Icon className="socials" icon="akar-icons:github-fill" onClick={() => { window.open("https://github.com/grrrrnt", "_blank") }} />
+                <Icon className="socials" icon="akar-icons:instagram-fill" onClick={() => { window.open("https://www.instagram.com/grrrr_nt", "_blank") }} />
+                <Icon className="socials" icon="akar-icons:spotify-fill" onClick={() => { window.open("https://smarturl.it/music-by-grant", "_blank") }} />
             </div>
             <div className="navbar-right">
                 <h2 className="navbar-right-item">Education</h2>
